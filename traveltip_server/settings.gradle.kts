2875606cd683ae1ad1cad6b8feca19d4736ca730
Shaked Golan <shaked518@gmail.com>
2025-04-03 16:45:25 +0300

    Java springboot with dependcies starter

---
+++ traveltip_server/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "traveltip_server"
---
+++ traveltip_server/src/main/java/com/traveltip/traveltip_server/TraveltipServerApplication.java
@@ -0,0 +1,13 @@
+package com.traveltip.traveltip_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveltipServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveltipServerApplication.class, args);
+	}
+
+}
